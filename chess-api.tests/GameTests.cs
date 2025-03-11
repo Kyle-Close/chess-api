9fda239ce37e639025a70b9886912010816ef2aa
Kyle-Close <k.james.close@gmail.com>
2025-03-10 22:58:15 -0700

    tests working in off branch

---
+++ chess-api.tests/GameTests.cs
@@ -0,0 +1,13 @@
+using Chess;
+namespace chess_api.tests;
+
+public class GameTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Game testGame = new Game();
+        
+        Assert.True(true);
+    }
+}
